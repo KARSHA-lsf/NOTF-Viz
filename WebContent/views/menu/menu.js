4113b6fc3bfaa7c254e1108d34fafa3daa0a8f7d
tharindu99 <madushanka.best@gmail.com>
2017-02-17 12:19:57 +0530

    menu added

---
+++ WebContent/views/menu/menu.js
@@ -0,0 +1,56 @@
+angular.module('HHF', [])
+    .directive('menu', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                loc: '@location',
+                menus: '='
+            },
+            link: function($scope, $element) {
+               
+            },
+            template:'<ul class="nav {{loc}}">'+
+	                 '<li ng-repeat="item in menus.MenuItems">'+
+	                 	'<a><i class="fa fa-home"></i>{{item.text}}<span class="fa fa-chevron-down"></span></a>'+
+	                    '<ul class="nav child_menu">'+
+	                        '<li ng-repeat="subitem in item.subItem">'+
+	                    		'<a href="{{subitem.link}}">{{subitem.text}}</a>'+
+	                    	'</li>'+
+	                    '</ul>'+
+	                  '</li>'           
+        };
+    })
+    
+    .controller('MenuCtrl', function ($scope) {
+        $scope.menus = {
+            "current": "index",
+            "MenuItems": [{
+                "active": true,
+                "text": "LMx",
+                "subItem":[{
+                	"text":"Permno",
+                	"link":"\/Permno"
+                	},
+                	{"text":"Date",
+                	"link":"\/Date"
+                	},
+                	{"text":"NAICS",
+                    	"link":"\/NAICS"
+                    	}
+                ]
+            },{
+                "active": true,
+                "text": "LMn",
+                "subItem":[{
+                	"text":"Permno",
+                	"link":"\/Permno"
+                	},
+                	{"text":"Date",
+                	"link":"\/Date"
+                	},
+                	{"text":"NAICS",
+                    	"link":"\/NAICS"
+                    	}
+                ]
+            }]};});
